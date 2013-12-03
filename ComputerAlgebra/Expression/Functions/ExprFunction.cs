4935cba132f3558c5709f4b37ae761fd2819cb0f
Dillon Sharlet <dsharlet@gmail.com>
2013-12-03 01:05:26 -0800

    Asserts body is non-null

--- ComputerAlgebra/Expression/Functions/ExprFunction.cs
+++ ComputerAlgebra/Expression/Functions/ExprFunction.cs
@@ -13,35 +13,45 @@ namespace ComputerAlgebra
     public class ExprFunction : Function
     {
         private Expression body;
+        /// <summary>
+        /// Get the body of this function.
+        /// </summary>
         public Expression Body { get { return body; } }
 
         private IEnumerable<Variable> parameters;
         public override IEnumerable<Variable> Parameters { get { return parameters; } }
 
-        private ExprFunction(string Name, Expression Body, IEnumerable<Variable> Params) : base(Name) { body = Body; parameters = Params; }
+        private ExprFunction(string Name, Expression Body, IEnumerable<Variable> Params) : base(Name) 
+        {
+            if (ReferenceEquals(Body, null))
+                throw new ArgumentNullException("Body");
 
+            body = Body; 
+            parameters = Params; 
+        }
+
+        /// <summary>
+        /// Create a new anonymous function defined by an expression.
+        /// </summary>
+        /// <param name="Body"></param>
+        /// <param name="Params"></param>
+        /// <returns></returns>
         public static ExprFunction New(Expression Body, IEnumerable<Variable> Params) { return new ExprFunction("<Anonymous>", Body, Params.Buffer()); }
-        public static ExprFunction New(string Name, Expression Body, IEnumerable<Variable> Params) { return new ExprFunction(Name, Body, Params.Buffer()); }
-
         public static ExprFunction New(Expression Body, params Variable[] Params) { return New(Body, Params.AsEnumerable()); }
+
+        /// <summary>
+        /// Create a new named function defined by an expression.
+        /// </summary>
+        /// <param name="Name"></param>
+        /// <param name="Body"></param>
+        /// <param name="Params"></param>
+        /// <returns></returns>
+        public static ExprFunction New(string Name, Expression Body, IEnumerable<Variable> Params) { return new ExprFunction(Name, Body, Params.Buffer()); }
         public static ExprFunction New(string Name, Expression Body, params Variable[] Params) { return New(Name, Body, Params.AsEnumerable()); }
 
-        public override Expression Call(IEnumerable<Expression> Args)
-        {
-            if (!ReferenceEquals(body, null))
-                return body.Evaluate(parameters.Zip(Args, (a, b) => Arrow.New(a, b)));
-            else
-                throw new UnresolvedName("Cannot call undefined function '" + Name + "'.");
-        }
+        public override Expression Call(IEnumerable<Expression> Args) { return body.Evaluate(parameters.Zip(Args, (a, b) => Arrow.New(a, b))); }
 
-        public override bool CanCall(IEnumerable<Expression> Args)
-        {
-            return CanCall() && parameters.Count() == Args.Count();
-        }
-
-        public override bool CanCall()
-        {
-            return !ReferenceEquals(body, null);
-        }
+        public override bool CanCall(IEnumerable<Expression> Args) { return parameters.Count() == Args.Count(); }
+        public override bool CanCall() { return true; }
     }
 }
