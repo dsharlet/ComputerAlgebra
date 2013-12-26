ba0f75ff4d0fb34c1eb0964e553f3cfb638181c1
Dillon Sharlet <dsharlet@gmail.com>
2013-12-26 01:08:35 -0800

    Fixed namespace names, added BigFloat/BigIntegerExtensions

---
+++ ComputerAlgebra/Utils/BigFloat.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Numerics;
+
+namespace ComputerAlgebra
+{
+    /// <summary>
+    /// Represent a rational number with System.Numerics.BigInteger as the numerator and denominator.
+    /// </summary>
+    //public struct BigFloat : IComparable<BigFloat>, IEquatable<BigFloat>, IFormattable
+    //{
+    //}
+}
---
+++ ComputerAlgebra/Utils/BigIntegerExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace ComputerAlgebra
+{
+    public static class BigIntegerExtensions
+    {
+        /// <summary>
+        /// Returns the trailing number of 0 digits in x.
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static int InsignificantDigits(this BigInteger x, int b)
+        {
+            BigInteger d = x;
+            for (int i = 0; ; ++i)
+            {
+                BigInteger r;
+                d = BigInteger.DivRem(d, b, out r);
+                if (!r.IsZero)
+                    return i;
+            }
+        }
+
+        public static int InsignificantDigits(this BigInteger x) { return InsignificantDigits(x, 10); }
+    }
+}
