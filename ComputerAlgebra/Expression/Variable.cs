e0bff12c4f92c07f7e1272ca422bc4ab8fab600e
Dillon Sharlet <dsharlet@gmail.com>
2014-01-20 23:06:52 -0800

    Added support for variables to be members of Rings

--- ComputerAlgebra/Expression/Variable.cs
+++ ComputerAlgebra/Expression/Variable.cs
@@ -10,13 +10,30 @@ namespace ComputerAlgebra
     /// </summary>
     public class Variable : NamedAtom
     {
-        protected Variable(string Name) : base(Name) { }
+        private Set ring;
+        /// <summary>
+        /// Identifies the ring this variable is a member of.
+        /// </summary>
+        public Set Ring { get { return ring; } }
+        
+        protected Variable(string Name, Set Ring) : base(Name) { ring = Ring; }
 
-        public static Variable New(string Name) { return new Variable(Name); }
+        /// <summary>
+        /// Create a new variable.
+        /// </summary>
+        /// <param name="Name"></param>
+        /// <returns></returns>
+        public static Variable New(string Name, Set Ring) { return new Variable(Name, Ring); }
+        /// <summary>
+        /// Create a new variable.
+        /// </summary>
+        /// <param name="Name"></param>
+        /// <returns></returns>
+        public static Variable New(string Name) { return new Variable(Name, Reals.New()); }
 
-        public override bool Matches(Expression E, MatchContext Matched)
-        {
-            return Matched.Matches(this, E);
+        public override bool Matches(Expression E, MatchContext Matched) 
+        { 
+            return (ReferenceEquals(Ring, null) || Ring.Contains(E)) && Matched.Matches(this, E); 
         }
         
         protected override int TypeRank { get { return 1; } }
@@ -29,15 +46,17 @@ namespace ComputerAlgebra
     {
         protected Func<Expression, bool> condition;
 
-        private PatternVariable(string Name, Func<Expression, bool> Condition) : base(Name) { condition = Condition; }
+        private PatternVariable(string Name, Set Ring, Func<Expression, bool> Condition) : base(Name, Ring) { condition = Condition; }
 
         /// <summary>
         /// Create a new variable with a condition callback for matching.
         /// </summary>
         /// <param name="Name"></param>
+        /// <param Name="Ring"></param>
         /// <param name="Condition">A function that should return true if the variable is allowed to match the given Expression.</param>
         /// <returns></returns>
-        public static PatternVariable New(string Name, Func<Expression, bool> Condition) { return new PatternVariable(Name, Condition); }
+        public static PatternVariable New(string Name, Set Ring, Func<Expression, bool> Condition) { return new PatternVariable(Name, Ring, Condition); }
+        public static PatternVariable New(string Name, Func<Expression, bool> Condition) { return new PatternVariable(Name, Reals.New(), Condition); }
 
         public override bool Matches(Expression E, MatchContext Matched)
         {
